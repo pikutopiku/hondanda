fcda34574b662e7707305bebf9384cafbd0ea336
pikuto <s1632119br@s.chibakoudai.jp>
2020-01-09 15:17:14 +0900

    URL,感情表示修正

--- books/models.py
+++ books/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.core.validators import URLValidator
+
 # Create your models here.
 
 
--- books/views.py
+++ books/views.py
@@ -25,59 +25,75 @@ def book_list(request):
 
     if (request.method == 'POST'):  # URLの送信ボタンが押された時のページ表示
         d = request.POST.get('url')
+        print(d.startswith('https://www.aozora.gr.jp/'))
+        if (d.startswith('https://www.aozora.gr.jp/') == True):
+            try:
+                res = urllib.request.urlopen(d)
+            except urllib.error.HTTPError as e:
+                print('raise HTTPError')
+                print(e.code)
+                print(e.reason)
+                params = {'data': data, 'alart': 2}
+            else:
+                html = urllib.request.urlopen(url=d)
+                soup = BeautifulSoup(html, "html.parser")
+                title = soup.find(class_='title').string
+                author = soup.find(class_='author').string
+                params = {'data': data, 'title': title,
+                          'author': author, 'alart': 0}
+                a_num = d.find('cards') + 6
+                s_num = d.find('files/')
+                m_num = d.find('_')
+                e_num = d.find('.html')
+                author_num = d[a_num:s_num - 1]
+                title_num = d[s_num+6:m_num]
+                all_num = d[m_num + 1:e_num]
+                f_name = d[s_num+6:e_num]
 
-        html = urllib.request.urlopen(url=d)
-        soup = BeautifulSoup(html, "html.parser")
-        title = soup.find(class_='title').string
-        author = soup.find(class_='author').string
-        params = {'data': data, 'title': title,
-                  'author': author, }
-        a_num = d.find('cards') + 6
-        s_num = d.find('files/')
-        m_num = d.find('_')
-        e_num = d.find('.html')
-        author_num = d[a_num:s_num - 1]
-        title_num = d[s_num+6:m_num]
-        all_num = d[m_num + 1:e_num]
-        f_name = d[s_num+6:e_num]
+                # 作品名，ID登録
+                title = TitleList(id=title_num, title=title)
+                title.save()
 
-        # 作品名，ID登録
-        title = TitleList(id=title_num, title=title)
-        title.save()
+                # 著者名，ID登録
+                author = AuthorList(id=author_num, author=author)
+                author.save()
 
-        # 著者名，ID登録
-        author = AuthorList(id=author_num, author=author)
-        author.save()
+                # 　本文ファイル保存
+                s1 = 'books/templates/books/text/' + f_name + '.txt'
+                if os.path.isfile(s1) is True:
+                    # print("あるよ")
+                    pass  # あるよ
+                else:
+                    # print("ないよ")
+                    head_tag = soup.head.prettify()
+                    body_tag = soup.body.prettify()
+                    html_tag = head_tag + body_tag
+                    f = open(s1, 'w')
+                    f.write(html_tag)
+                    f.close()
 
-        # 　本文ファイル保存
-        s1 = 'books/templates/books/text/' + f_name + '.txt'
-        if os.path.isfile(s1) is True:
-            pass  # あるよ
+                # Bookテーブル登録
+                book = Book(id=all_num, titlelist=TitleList(id=title_num),
+                            authorlist=AuthorList(id=author_num), url=f_name, )
+                book.save()
+
+                querySet = Bookshelf.objects.filter(
+                    user_id=user, book_id=all_num)
+
+                if querySet.first() is None:
+
+                    # 本棚テーブル登録
+                    bookshelf = Bookshelf(user=User(id=user),
+                                          book=Book(id=all_num))
+                    bookshelf.save()
+        elif(len(d) >= 1):
+            params = {'data': data, 'alart': 1}
+            print(d)
         else:
-            # print("ないよ")
-            head_tag = soup.head.prettify()
-            body_tag = soup.body.prettify()
-            html_tag = head_tag + body_tag
-            f = open(s1, 'w')
-            f.write(html_tag)
-            f.close()
-
-        # Bookテーブル登録
-        book = Book(id=all_num, titlelist=TitleList(id=title_num),
-                    authorlist=AuthorList(id=author_num), url=f_name, )
-        book.save()
-
-        querySet = Bookshelf.objects.filter(user_id=user, book_id=all_num)
-
-        if querySet.first() is None:
-
-            # 本棚テーブル登録
-            bookshelf = Bookshelf(user=User(id=user),
-                                  book=Book(id=all_num))
-            bookshelf.save()
+            params = {'data': data, 'alart': 0}
 
     else:  # ログインからのページ表示
-        params = {'data': data, }
+        params = {'data': data, 'alart': 0}
 
     return render(request, 'books/book_list.html', params)
 
@@ -87,12 +103,29 @@ def book_text(request, u):  # u=bookid
 
     user = request.user.id
     k = Book.objects.get(id=u)
+    d = Dialog.objects.filter(user=User(id=user), book=Book(id=u))
+    d1 = d.values_list('dialog', flat=True)
+    d1 = list(d1)
+    d2 = d.values_list('emotionID_id', flat=True)
+    d2 = list(d2)
+    dd1 = {}
+    dd2 = {}
+    for i in range(len(d1)):
+        print(i)
+        dd1[i] = d2[i]
+        dd2[i] = d2[i]
+
+    print(d1)
+    print(d2)
+    print(dd1)
+    print(dd2)
 
     url = 'books/templates/books/text/' + k.url + '.txt'
     text_data = open(url, 'r')
     text = text_data.read()
     text_data.close()
-    params = {'data': text, 'user': user, 'bookID': u, }
+    params = {'data': text, 'user': user,
+              'bookID': u, 'emo1': dd1, 'emo2': dd2, 'len': len(d1)}
 
     return render(request, 'books/book_text.html', params)
 
@@ -136,7 +169,9 @@ def count_emotion(request):
     if (request.method == 'GET'):
         d = request.GET.get('Dialogid')
         b = request.GET.get('Bookid')
+        u = request.GET.get('User')
         log_db = Dialog.objects.filter(dialog=d, book_id=b)
+        log_db = log_db.exclude(user_id=u)
 
         if not (log_db.count() == 0):
             response = log_db
@@ -146,40 +181,45 @@ def count_emotion(request):
                       '嫌悪', '怒り', '期待', '無し']
             colors = ['pink', 'palegreen', 'limegreen', 'lightcyan', 'lightskyblue',
                       'mediumorchid', 'tomato', 'orange', 'silver']
+            Not = 0
 
             # count
-            emo = []
             for i in range(9):
                 k = i + 1
-                emo = log_db.filter(emotionID_id=k,)
                 datas[i] = log_db.filter(emotionID_id=k).count()
+            Not = log_db.filter(emotionID_id=10).count()
 
-            # # create figure
-            import matplotlib
-            matplotlib.use('Agg')  # <= これが必要
-            import matplotlib.pyplot as plt
-            import matplotlib.cm as cm
-            import numpy as np
-            import japanize_matplotlib
+            if (sum(datas) > 1):
+                # # create figure
+                import matplotlib
+                matplotlib.use('Agg')  # <= これが必要
+                import matplotlib.pyplot as plt
+                import matplotlib.cm as cm
+                import numpy as np
+                import japanize_matplotlib
 
-            # 綺麗に書くためのおまじない###
-            plt.style.use('ggplot')
-            plt.rcParams.update({'font.size': 15})
+                # 綺麗に書くためのおまじない###
+                plt.style.use('ggplot')
+                plt.rcParams.update({'font.size': 15})
 
-            # 各種パラメータ###
-            size = (6, 5)  # 凡例を配置する関係でsizeは横長にしておきます。
+                # 各種パラメータ###
+                size = (6, 5)  # 凡例を配置する関係でsizeは横長にしておきます。
 
-            # pie###
-            plt.figure(figsize=size, dpi=100)
-            plt.pie(datas, colors=colors, counterclock=False, startangle=90,
-                    autopct=lambda p: '{:.1f}%'.format(p) if p >= 5 else '')
-            plt.subplots_adjust(left=0, right=0.7)
+                # pie###
+                plt.figure(figsize=size, dpi=100)
+                plt.pie(datas, colors=colors, counterclock=False, startangle=90,
+                        autopct=lambda p: '{:.1f}%'.format(p) if p >= 5 else '')
+                plt.subplots_adjust(left=0, right=0.7)
 
-            plt.legend(labers, fancybox=True, loc='center left',
-                       bbox_to_anchor=(0.9, 0.5))
-            plt.axis('equal')
-            plt.savefig('books/static/figure.png',
-                        bbox_inches='tight', pad_inches=0.05)
+                plt.legend(labers, fancybox=True, loc='center left',
+                           bbox_to_anchor=(0.9, 0.5))
+                plt.axis('equal')
+                plt.savefig('books/static/figure.png',
+                            bbox_inches='tight', pad_inches=0.05)
+
+            else:
+                print("感情を選択している人がいません．")
+                response = 0
 
             return HttpResponse(response)
 
